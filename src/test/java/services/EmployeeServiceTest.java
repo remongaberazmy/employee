8de2b7f61872be438d1a3985d601b6a03623d8e5
TRADENET\rgaber <rgaber@kn-it.com>
2025-01-04 17:55:26 +0200

    Adding a few test cases

--- src/test/java/services/EmployeeServiceTest.java
+++ src/test/java/services/EmployeeServiceTest.java
@@ -1,8 +1,15 @@
 package services;
 
 import org.digivisions.Services.EmployeeService;
+import org.digivisions.models.BaseModel;
+import org.digivisions.models.EmployeeDTO;
+import org.digivisions.utils.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.util.AssertionErrors.assertEquals;
 
 @SpringBootTest
 public class EmployeeServiceTest {
@@ -10,5 +17,15 @@ public class EmployeeServiceTest {
 	@Autowired
 	private EmployeeService employeeService;
 
+	void testCreate(){
+		EmployeeDTO employeeDTO = new EmployeeDTO();
+		employeeDTO.setFirstName("Test");
+		employeeDTO.setLastName("Test");
+		employeeDTO.setEmail("test@test.com");
+		employeeDTO.setDepartment("Test");
+		employeeDTO.setSalary(BigDecimal.ZERO);
 
+		BaseModel baseModel = employeeService.create(employeeDTO);
+		assertEquals(Constants.CREATED, baseModel.getReplyMessage(), "Employee should be created");
+	}
 }
